9b4d72259057fee4a7b8f07ae06b9781bb6a328a
yults <yultsova@niuitmo.ru>
2022-10-03 19:35:43 +0300

    Lecture1

---
+++ app/Lecture1.hs
@@ -0,0 +1,152 @@
+module Lecture1 (main) where
+import Numeric.Natural
+-- Haskell ---------------------------------
+--   GHC - компилятор
+--   stack - чтобы сбилдить 
+--   cabal - мета информация 
+--   snapshot - набор стабильных версий всех библиотек (избавление от dependency hell)
+--   статический, иммутабельный (не именяемые переменные)
+--   purity, non-null, ленивые вычисления
+--   чистые функции (зависят только от своих аргументов, не изменяют глобальное
+--   состояние, отрабатывают одинаково на одних и тех же переменных)
+--   Исходный файл - модуль. Имя модуля = полный путь файла. Из модулей можно собрать package
+--   Создать проект stack new newProject
+
+-- GHCI ------------------------------------
+--   типа repl, интерактивная среда
+--   :q . *Empty> :q Leaving GHCi -- выход
+--   Загрузить из файла в ghci 
+--   :l Lecture1.hs
+--   :t - узнать тип, в ghci чтобы видеть всегла :set +t
+
+-- Установка -------------------------------
+--   |Идем на сайт ghcup - там скачиваем. Если возникает проблема с curl, на сайте 
+--   вкладка Troubleshooting. Отключить антивирус перед скачкой, могут возникнуть
+--   проблемы с сертификатом. Далее когда будет успешно, идем в VS code, находим
+--   расширение которое зависит от ghcup (называется просто - Haskell). Там есть инструкция,
+--   сделать как там. Вкратце - зайти в переменные среды, занести в Path stack, cabal
+
+import Lib
+
+-- Основы -----------------------------------
+--   2 + 2 /= 5 не равно
+--   div 7 3 + 1 = 3 у функции высший приоритет
+--   Prefix функции и операторы в скобках :
+--   min 1 2, (+) 3 7
+--   Infix операторы и функции в `` :
+--   1 + 2, 7 `mod` 3
+--   "Hello " ++ "world!" конкатенация строк
+
+-- Обьявление функций -----------------------
+addMul :: Int -> Int -> Int -> Int -- 3 аргумента и результат
+addMul x y z = x + y * z
+greet :: String -> String
+greet name = "Hello, " ++ name ++ "!"
+main :: IO ()
+main = putStrLn (greet "Tus") -- console.log
+
+-- Собственные операторы --------------------
+--   infix|infixr|infixl приоритет (0; 10] название оператора
+infixr 1 ==> -- обьявление импликации (правоассоциативная приоритет 1 имя ==>)
+(==>) ::   Bool -> Bool -> Bool 
+a ==> b = not a || b
+--   правоассоциативная значит скобки расставляются справа налево т.е с конца
+--   space приоритет 10, || 2, && 3
+--   infixl : a * b * C * d === ((a * b) * c) * d
+--   infixr : a * b * C * d === a * (b * (c * d))
+--   infix : a * b * C * d === error
+
+-- Let VS Where------------------------------ https://wiki.haskell.org/Let_vs._Where
+-- Let -----------
+sumSquare :: Int -> Int -> Int 
+sumSquare x y = x^2 + y^2
+--   with let
+sumSquareLet :: Int -> Int -> Int 
+sumSquareLet x y = let x2 = x ^ 2 
+                       y2 = y ^ 2
+                    in x2 + y2 
+-- Where -----------
+sumSquareWhere :: Double -> Double -> Double
+sumSquareWhere a b = a2 + b2
+  where  -- details of implementation are inside 'where'
+    square x = x ^ 2
+    a2       = square a
+    b2       = square b
+-- If --------------------------------
+factorial :: Integer -> Integer  --длинная арифметика 
+factorial n = if n <= 1 
+              then 1 
+              else n * factorial (n - 1)  
+-- Guards ----------------------------
+-- длинные if else типа case
+collatzSum :: Natural -> Natural
+collatzSum n
+  | n == 1    = 1
+  | even n    = n + collatzSum (n `div` 2)
+  | otherwise = n + collatzSum (3 * n + 1)
+-- Case ------------------------------
+getFont :: Int -> String
+getFont n = case n of
+                 0 -> "PLAIN"
+                 1 -> "BOLD"
+                 2 -> "ITALIC"
+                 _ -> "UNKNOWN"
+
+caseOperation 
+  :: Char -> Int -> Int -> Int
+caseOperation op x y = 
+    case op of
+         '+' -> x + y
+         '-' -> x - y
+         _   -> 0   -- _ should be 
+                    -- under ' and 
+                    -- not under -
+--  caseOperation '+' 1 2 -- 3
+-- Типы ------------------------------------
+--  List ------ 
+--   [1 + 2, 3 + 4, 5 * 6]
+--   let list = [2, 1, 3]
+--   [5, 10] ++ list -- [5, 10, 2, 1, 3]
+--   10 : list -- добавить 10 в начало  листа
+--   reverse list -- [3, 1, 2]
+emptyList :: [Int] -- обЪявлние const
+emptyList = []
+singletonList :: [Int]
+singletonList = 1 : emptyList
+listExample :: [Int]
+listExample = [2, 1, 3]
+listExample2 = 5:10:listExample -- [5, 10, 2, 1, 3]
+twoLists = singletonList ++ listExample -- конкатенация [1, 2, 1, 3]
+treeLists = singletonList ++ [7] ++ listExample -- [1, 7, 1, 2, 1, 3]
+string :: [Char]
+string = "str" -- ['s', 't', 'r']
+--   "" == [] True
+--  Ranges -----
+--   [0 .. 5] -- [0, 1, 2, 3, 4, 5]
+--   [1, 3 .. 7] -- [1, 3, 5, 7]
+--   [0..] -- [0, 1, 2, 3 ...] infinite list
+--   [0, 2 ..] -- [0, 2, 4, 6 ...] all even numbers
+--   [5, 4, .. 1] --[5, 4, 3, 2, 1] - обратная последовательность
+--   [5 .. 1] -- [] ошибка, нужен явный шаг
+--   тут могут быть любые упорядочевыемые типы
+--  Lists Functions---
+--   Часть 1
+--   let l = [2, 1, 3]
+--   head l -- 2
+--   tail l -- [1, 3]  
+--   last l -- 3
+--   init l -- [2, 1]
+--   Типа иллюстация
+--   head taaaaail
+--   iniiiiit last
+--   конец иллюстрации
+--   все что выше лучше не использовать, потому что падает на пустом
+--   Часть 2 Всяко разное
+--   drop 2 [2, 1, 3] - выкинкуть последние элементы списка (2 штуки тут) -- [1, 3]
+--   take 1 [2, 1, 3] - берет первые элементы списка (2 штуки тут) -- [2, 1]
+--   replicate 3 [1..5]  - повторить -- [[1..5], [1..5], [1..5]]
+--   zip [1, 2, 3] "abc" - создать список пар -- [(1,'a'),(2,'b'),(3,'c')]
+--   unzip [(5, True), (10, False)] -- ([5,10],[True,False])
+--   words "Hello,    Haskell \t\n\n   world!" - типа сплит скипWS -- ["Hello,","Haskell","world!"]
+--   unwords ["Hello,","Haskell","world!"] -- "Hello, Haskell world!"
+-- 
---
+++ app/Style.hs
@@ -0,0 +1,208 @@
+-- Рекомендации к стилизации кода
+
+
+
+--  ПРОБЕЛЫ И ПУСТЫЕ СТРОКИ
+--   Длина строки 80. Но иногда можно 100
+--   Табы нельзя но можно 2 пробела примеры
+sayHello :: IO ()
+sayHello = do
+  name <- getLine
+  putStrLn $ greeting name
+  where
+    greeting name = "Hello, " ++ name ++ "!"
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter _ [] = []
+filter p (x:xs)
+  | p x       = x : filter p xs
+  | otherwise = filter p xs
+--   1 пустая строка между определениями верхнего уровня.
+--   0 пустых строк между сигнатурами типов и определениями функций.
+--   | 1 пустая строка между определениями в объявлении экземпляра класса типов или внутри where
+--   предложения или let блока, если определения большие. (что)
+--   Можно добавить пустые строки внутрь большого do блока, чтобы отделить его логические части.
+--   Можно выравнивать блоки кода дополнительными пробелами, если выравнивание подчеркивает общую структуру 
+--   Пример
+--
+data WalletApiRecord route = WalletApiRecord
+  { _test      :: route :- WTestApi             -- /test
+  , _wallets   :: route :- WWalletsApi          -- /wallets
+  , _accounts  :: route :- WAccountsApi         -- /accounts
+  , _addresses :: route :- WAddressesApi        -- /addresses
+  , _profile   :: route :- WProfileApi          -- /profile
+  -- ...
+  } deriving Generic
+--  
+--   Окружать бинарные операторы 1 пробелом обеих сторон: 3 + 5.
+--   Можно не окружать, чтобы подчеркнуть группировку терминов: 2 + 2*2
+--   При использовании каррирования(что) с бинарными операторами необходимо добавить 1 пробел между аргументом и операцией: (42 +)
+--   Нужно удалить все конечные пробелы и добавить завершающую новую строку ко всем исходным файлам.
+--   сюда https://editorconfig.org/
+
+--  ИМЕНА
+--   lowerCamelCase for functions, variables, and global constants.
+--   UpperCamelCase for types.
+--   Не использовать короткие имена, если значение не ясно из контекста
+--   | НЕ надо использовать все буквы в аббревеатуре заглавными, например
+--   лучше HttpServer вместо HTTPServer. Исключения - 2 или 3 буквенные типа IO, STM
+
+--  RECORDS
+--   я не поняла
+
+
+--  КОММЕНТАРИИ
+--   Длинные комментарии с правильной пунктуацией
+--   Пишем сигнатуру типа для каждого определения верхнего уровня.
+--   Комментируем каждую экспортируемую функцию и тип данных.
+--   Комментируем каждую функцию верхнего уровня.
+--   You must use Haddock syntax in the comments. (что)
+--   какой-то пример
+-- | Send a message on a socket. The socket must be in a connected
+-- state. Returns the number of bytes sent. Applications are
+-- responsible for ensuring that all data has been sent.
+send
+  :: Socket      -- ^ Connected socket
+  -> ByteString  -- ^ Data to send
+  -> IO Int      -- ^ Bytes sent
+--   По комменту должно быть понятно о чем функция
+-- | Person representation used in address book.
+data Person = Person
+  { age  :: Int     -- ^ Age
+  , name :: String  -- ^ First name
+  }
+--   Длинные комменты
+data Record = Record
+  { -- | This is a very very very long comment that is split over
+    -- multiple lines.
+    field1 :: Text
+
+    -- | This is a second very very very long comment that is split
+    -- over multiple lines.
+  , field2 :: Int
+  }
+--   Отделять комментарии в конце строки от кода двумя пробелами.
+--   Выравнивать комментарии для определений типов данных. 
+data Parser = Parser
+  Int         -- Current position
+  ByteString  -- Remaining input
+
+foo :: Int -> Int
+foo n = salt * n + 9
+  where
+    salt = 453645243  -- Magic hash salt
+
+--  ССЫЛКИ
+--   нужно делать но если это может кому то быть нужным и только 1 раз
+
+----------------------------------
+-- Если не сложилось с stylish
+--   | Напишите каждую LANGUAGEпрагму на отдельной строке, отсортируйте их по алфавиту 
+--   и выровняйте по максимальной ширине среди них. 
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types       #-}
+{-# LANGUAGE TemplateHaskell  #-}
+--   | Используйте единственное число при именовании модулей (например, используйте Data.Mapand
+--   Data.ByteString.Internalвместо Data.Mapsand Data.ByteString.Internals).
+--   Иногда допустимо использовать множественное число (например Types, , Instances).
+--   Модули должны иметь явный экспорт
+module Data.Set
+  ( -- * The @Set@ type
+    Set
+  , empty
+  , singleton
+
+   -- * Querying
+  , member
+  ) where
+--   отступ в 2 пробела для списка экспорта
+--   отсортировать каждый раздел в алфавитном порядке. Однако в каждом разделе классы, типы данных и псевдонимы типов должны быть написаны перед функциями.
+--   https://hackage.haskell.org/package/weeder штука чтобы убрать неиспользуемые импорты
+--   остальное по импортам сложно 
+
+-- ОБЪЯВЛЕНИЕ ДАННЫХ
+--   Выровняйте конструкторы в определении типа данных. Примеры:
+data Tree a
+  = Branch a (Tree a) (Tree a)
+  | Leaf
+
+data HttpException
+  = InvalidStatusCode Int
+  | MissingContentHeader
+
+data Person = Person
+  { firstName :: String  -- ^ First name
+  , lastName  :: String  -- ^ Last name
+  , age       :: Int     -- ^ Age
+  } deriving (Eq, Show)
+
+--   Все функции верхнего уровня должны иметь сигнатуры типов.
+--   Все функции внутри where должны иметь сигнатуры типов. 
+--   Выравнивания если сигнатура длинная
+putValueInState
+  :: (MonadIO m, WithLogger m)
+  => UserState
+  -> Maybe Int
+  -> AppConfig
+  -> (Int -> m ())
+  -> m ()
+--   Много ограничений
+parseTree
+  :: ( KnownSpine components
+     , AllConstrained (ComponentTokenizer components) components
+     , AllConstrained (ComponentTokenToLit components) components
+     )
+  => Text
+  -> Either (ParseError components) (Expr ParseTreeExt CommandId components)
+--   Имена переменных длинные
+putValueInState
+  userState
+  mValue@(Just x)
+  Config{..}        -- { should go after ctor name without space
+  valueModificator
+    = do               -- note how this line uses 4-space indentation
+  <code goes here>
+--   Комментарий оператора
+-- | Append a piece to the URI.
+infixl 5 />
+(/>) :: Uri -> PathPiece -> Uri
+--  ПРАГМЫ хз
+--  Список объявлений
+numbers = [1, 2, 4]
+
+exceptions =
+  [ InvalidStatusCode
+  , MissingContentHeader
+  , InternalServerError
+  ]
+--   НЕ вставлять пробел после лямбды.
+bar :: IO ()
+bar =
+  forM_ [1, 2, 3] $ \n -> do
+    putStrLn "Here comes a number!"
+    print n
+
+foo :: IO ()
+foo =
+  alloca 10 $ \a ->
+  alloca 20 $ \b ->
+  cFunction a b
+--   if else без выпендрежа
+foo =
+  if ...
+  then ...
+  else ...
+--   с лямдами
+foo =
+  bar $ \qux -> if predicate qux
+    then doSomethingSilly
+    else someOtherCode
+--   с do
+foo = do
+  someCode
+  if condition
+  then do
+    someMoreCode
+    andMore
+  else  -- you _may_ omit the `do` if the block is a one-liner
+    return ()
